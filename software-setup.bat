60b3871abef0d20bf48818aa428b53c2dbc9e913
Gregg Marshall <gmarshall@vendor-tech.com>
2016-12-26 10:13:24 -0700

    Adding initial scripts and composer.json for Behat Drupal extension

---
+++ software-setup.bat
@@ -0,0 +1,83 @@
+:: Manually install cmdr from http://cmder.net/
+:: Manually install Dev Desktop from https://dev.acquia.com/downloads, programs to c:\DevDesktop\programs, sites to c:\DevDesktop\drupalsites
+:: Manually install Chocolately using @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+:: Install Node.js and NPM
+cinst nodejs -y
+cinst npm -y
+:: Instal 7zip
+cinst 7zip -y
+:: Install PHPStorm
+cinst phpstorm -y
+:: Install Atom as on editor option
+cinst Atom -y
+:: Install Notepad++ as lightweight editor
+cinst notepadplusplus -y
+:: Install nano for sysadmin
+cinst nano -y
+:: Install Google Chrome and Firefox
+cinst GoogleChrome -y
+cinst Firefox -y
+:: Install VirtualBox and Extensions
+cinst virtualbox -y
+cinst VirtualBox.ExtensionPack -y
+:: Install vagrant
+cinst vagrant -y
+:: Install docker
+cinst docker -y
+:: Install Filezilla for FTP
+cinst filezilla -y
+:: Install Gimp
+cinst gimp -y
+:: Manually install memcached, using instructions at https://commaster.net/content/installing-memcached-windows, copy files into c:\cmdr\bin
+:: Install ESLint
+npm install -g eslint
+:: Edit the composer.bat and drush.bat batch files in C:\DevDesktop\Programs\tools to use fully qualified path to php programs
+::    php.exe C:\DevDesktop\Programs\tools\composer.phar %*
+::    @php.exe "C:\DevDesktop\Programs\tools\vendor\drush\drush\drush.php" --php="php.exe" %*
+:: Install Composer GCR to resolve --global issues
+:: see https://pantheon.io/blog/fixing-composer-global-command
+composer global require consolidation/cgr
+:: Create a file C:\DevDesktop\Programs\tools\cgr.bat containing
+:: C:\DevDesktop\Programs\php5_5\php.exe %HOME%\AppData\Roaming\Composer\vendor\consolidation\cgr\bin\cgr %*
+:: Install Behat
+cgr --dev behat/behat
+:: Install Mink
+cgr behat/mink
+# Global installation of Drupal Extension
+cd %HOME%\.composer\global\behat
+mkdir drupalextension
+cd drupalextension/
+cat << EOT > composer.json
+{
+  "require": {
+    "drupal/drupal-extension": "~3.0"
+},
+  "config": {
+    "bin-dir": "bin/"
+  }
+}
+EOT
+composer install
+ln -s /opt/drupalextension/bin/behat /usr/local/bin/behat
+cd ~
+:: Create a file C:\DevDesktop\Programs\tools\behat.bat containing
+:: setlocal DISABLEDELAYEDEXPANSION
+:: C:\DevDesktop\Programs\php5_5\php.exe %HOME%\.composer\global\behat\drupalextension\vendor\behat\behat\bin\behat %*
+:: Install Copy and Paste Detector
+cgr "sebastian/phpcpd=*"
+:: Create a file C:\DevDesktop\Programs\tools\phpcpd.bat containing
+:: C:\DevDesktop\Programs\php5_5\php.exe %HOME%\.composer\global\sebastian\phpcpd\vendor\sebastian\phpcpd\composer\bin\phpcpd %*
+:: Install Coder and PHPCS
+cgr "drupal/coder"
+:: Create a file C:\DevDesktop\Programs\tools\phpcs.bat containing
+:: C:\DevDesktop\Programs\php5_5\php.exe %HOME%\.composer\global\drupal\coder\vendor\squizlabs\php_codesniffer\scripts\phpcs %*
+phpcs --config-set installed_paths %HOME%/.composer/vendor/drupal/coder/coder_sniffer
+:: Install Drupal Code Review (may not be compatible with Windows)
+cgr alexdesignworks/dcr
+:: Create a file C:\DevDesktop\Programs\tools\dcr.bat containing
+:: C:\DevDesktop\Programs\php5_5\php.exe %HOME%\.composer\global\drupal\coder\vendor\squizlabs\php_codesniffer\scripts\phpcs %*
+:: Install Drupal Console for Drupal 8
+cinst SQlite -y
+cgr drupal/console:@stable
+:: Create a file C:\DevDesktop\Programs\tools\drupal.bat containing
+:: C:\DevDesktop\Programs\php5_5\php.exe %HOME%\.composer\global\drupal\console\vendor\drupal\console\bin\console %*
---
+++ software-setup.sh
@@ -0,0 +1,106 @@
+# XCode Command Line Tools
+xcode-select --install
+read -n1 -r -p "Let XCode finish installing, then press space to continue..." key
+# HomeBrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# HomeBrew Cask
+brew install caskroom/cask/brew-cask
+# Node.js
+brew install node
+# PHPStorm
+brew cask install phpstorm
+# Atom as lightweight editor
+brew cask install atom
+# Acquia Dev Desktop
+brew cask install acquia-dev-desktop
+# VirtualBox
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+# Vagrant
+brew cask install vagrant
+# Docker
+brew cask install docker
+# Cyberduck FTP
+brew cask install cyberduck
+# Firefox
+brew cask install firefox
+# Chrome
+brew cask install google-chrome
+# Gimp
+brew cask install gimp
+# Memcached
+brew install memcached
+# ESLint
+sudo npm install -g eslint
+# Composer
+brew install homebrew/php/composer
+# Composer GCR to resolve --global issues
+# see https://pantheon.io/blog/fixing-composer-global-command
+composer global require consolidation/cgr
+# Skitch
+brew cask install skitch
+# Colloquy IRC Client
+brew cask install colloquy
+# MacDown
+brew cask install macdown
+# Pandoc universal document converter
+brew install pandoc
+# Basic TeX for PDF output from pandoc
+brew cask install basictex
+# Additional fonts
+tlmgr install collection-fontsrecommended
+# Install Drush 6, 7 and 8 with 8 being default
+#    remove any existing copy of Drush
+brew remove --force drush
+cgr drush/drush:8.*
+# Drush 8
+mkdir ~/drush8
+cd ~/drush8
+composer require drush/drush:dev-master
+# Drush 7
+mkdir ~/drush7
+cd ~/drush7
+composer require "drush/drush:7.*"
+# Drush 6
+mkdir ~/drush6
+cd ~/drush6
+composer require "drush/drush:6.*"
+# add Drush aliases (and make Composer globals available in path)
+sudo cat << EOT >> ~/.bash_profile
+alias drush6='~/drush6/vendor/bin/drush'
+alias drush7='~/drush7/vendor/bin/drush'
+alias drush8='~/drush8/vendor/bin/drush'
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+EOT
+cd ~
+source .bash_profile
+# Behat
+cgr --dev behat/behat
+# Mink
+cgr behat/mink
+# Global installation of Drupal Extension
+cd /opt/
+sudo mkdir drupalextension
+cd drupalextension/
+cat << EOT > composer.json
+{
+  "require": {
+    "drupal/drupal-extension": "~3.0"
+},
+  "config": {
+    "bin-dir": "bin/"
+  }
+}
+EOT
+composer install
+ln -s /opt/drupalextension/bin/behat /usr/local/bin/behat
+cd ~
+# Copy and Paste Detector
+cgr "sebastian/phpcpd=*"
+# Coder and PHPCS
+cgr "drupal/coder"
+phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
+# Drupal Code Review
+cgr alexdesignworks/dcr
+# Drupal Console
+cgr drupal/console:@stable
